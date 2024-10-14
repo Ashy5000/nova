1889ba043713bec93cbdb94d8181c6474df1c1fb
Ashy5000 <ashydashy5000@gmail.com>
2024-10-14 10:08:05 -0700

    Implements uNOVA balance tracker

--- frontend/nova/app/app/unova_manager.tsx
+++ frontend/nova/app/app/unova_manager.tsx
@@ -17,123 +17,151 @@ export default function UNOVAManager(props) {
   }
 
   return (
-    <div className="flex justify-evenly">
-      <div className="dark:invert backdrop-brightness-95 rounded-sm p-5 m-12 w-screen min-h-96">
-        <div className="backdrop-blur-md backdrop-brightness-50 rounded-sm p-3">
-          <b>Wrap</b>
-          <br />
-          Deposit your ETH, AAVE, and MKR into a contract to mint new uNOVA.
-          <br />
-          <i>Fixed rate</i>
-        </div>
-        <div className="backdrop-blur-md backdrop-brightness-50 rounded-sm p-3 my-2">
-          <img src="eth.png" className="inline w-6 m-2"></img>
-          <input
-            className="w-36 my-2 bg-gray-800 rounded-sm p-1"
-            type="number"
-            value={stakeEth}
-            onChange={(e) => {
-              if (isNaN(e.target.value) || isNaN(parseFloat(e.target.value))) {
-                return;
-              }
-              setStakeEth(e.target.value);
-            }}
-          />
-          &nbsp; <b>ETH</b>&nbsp;&nbsp;&nbsp;&nbsp;
-        </div>
-        <div className="backdrop-blur-md backdrop-brightness-50 rounded-sm p-3 my-2">
-          <img src="aave.svg" className="inline w-6 m-2"></img>
-          <input
-            className="w-36 my-2 bg-gray-800 rounded-sm cursor-not-allowed p-1"
-            type="number"
-            value={stakeEth * 10}
-          />
-          &nbsp; <b>AAVE</b>&nbsp;
-        </div>
-        <div className="backdrop-blur-md backdrop-brightness-50 rounded-sm p-3 my-2">
-          <img src="mkr.svg" className="inline w-6 m-2"></img>
-          <input
-            className="w-36 my-2 bg-gray-800 rounded-sm cursor-not-allowed p-1"
-            type="number"
-            value={stakeEth}
-          />
-          &nbsp; <b>MKR</b>&nbsp;&nbsp;
-        </div>
-        <div className="flex justify-center">
-          <img src="arrow.svg" width={50} height={50} className="opacity-70" />
-        </div>
-        <div className="backdrop-blur-md backdrop-brightness-50 rounded-sm p-3 my-2">
-          <input
-            className="w-36 my-2 bg-gray-800 rounded-sm cursor-not-allowed p-1"
-            type="number"
-            value={stakeEth / 0.002}
-          />
-          &nbsp; <b>uNOVA</b>
-        </div>
+    <div>
+      <br />
+      <div className="backdrop-brightness-50 rounded-sm w-min p-3 ml-12">
+        <mark className="dark:invert bg-foreground">Balance:</mark>
         <br />
-        <button
-          className="backdrop-blur-md backdrop-brightness-90 py-1 w-64 rounded-sm"
-          onClick={wrap}
-        >
-          <mark className="bg-foreground">Wrap</mark>
-        </button>
+        <mark className="dark:invert bg-foreground text-2xl">
+          <b>100</b>
+        </mark>
+        <br />
+        <mark className="dark:invert bg-foreground">uNOVA</mark>
       </div>
-      <div className="dark:invert backdrop-brightness-90 rounded-sm p-5 m-12 w-screen min-h-96">
-        <div className="backdrop-blur-md backdrop-brightness-50 rounded-sm p-3">
-          <b>Unwrap</b>
+      <div className="flex justify-evenly">
+        <div className="dark:invert backdrop-brightness-95 rounded-sm p-5 m-12 w-screen min-h-96">
+          <div className="backdrop-blur-md backdrop-brightness-50 rounded-sm p-3">
+            <b>Wrap</b>
+            <br />
+            Deposit your ETH, AAVE, and MKR into a contract to mint new uNOVA.
+            <br />
+            <i>Fixed rate</i>
+          </div>
+          <div className="backdrop-blur-md backdrop-brightness-50 rounded-sm p-3 my-2">
+            <img src="eth.png" className="inline w-6 m-2"></img>
+            <input
+              className="w-36 my-2 bg-gray-800 rounded-sm p-1"
+              type="number"
+              value={stakeEth}
+              onChange={(e) => {
+                if (
+                  isNaN(e.target.value) ||
+                  isNaN(parseFloat(e.target.value))
+                ) {
+                  return;
+                }
+                setStakeEth(e.target.value);
+              }}
+            />
+            &nbsp; <b>ETH</b>&nbsp;&nbsp;&nbsp;&nbsp;
+          </div>
+          <div className="backdrop-blur-md backdrop-brightness-50 rounded-sm p-3 my-2">
+            <img src="aave.svg" className="inline w-6 m-2"></img>
+            <input
+              className="w-36 my-2 bg-gray-800 rounded-sm cursor-not-allowed p-1"
+              type="number"
+              value={stakeEth * 10}
+            />
+            &nbsp; <b>AAVE</b>&nbsp;
+          </div>
+          <div className="backdrop-blur-md backdrop-brightness-50 rounded-sm p-3 my-2">
+            <img src="mkr.svg" className="inline w-6 m-2"></img>
+            <input
+              className="w-36 my-2 bg-gray-800 rounded-sm cursor-not-allowed p-1"
+              type="number"
+              value={stakeEth}
+            />
+            &nbsp; <b>MKR</b>&nbsp;&nbsp;
+          </div>
+          <div className="flex justify-center">
+            <img
+              src="arrow.svg"
+              width={50}
+              height={50}
+              className="opacity-70"
+            />
+          </div>
+          <div className="backdrop-blur-md backdrop-brightness-50 rounded-sm p-3 my-2">
+            <input
+              className="w-36 my-2 bg-gray-800 rounded-sm cursor-not-allowed p-1"
+              type="number"
+              value={stakeEth / 0.002}
+            />
+            &nbsp; <b>uNOVA</b>
+          </div>
           <br />
-          Burn uNOVA in a contract to receive ETH.
+          <button
+            className="backdrop-blur-md backdrop-brightness-90 py-1 w-64 rounded-sm"
+            onClick={wrap}
+          >
+            <mark className="bg-foreground">Wrap</mark>
+          </button>
+        </div>
+        <div className="dark:invert backdrop-brightness-90 rounded-sm p-5 m-12 w-screen min-h-96">
+          <div className="backdrop-blur-md backdrop-brightness-50 rounded-sm p-3">
+            <b>Unwrap</b>
+            <br />
+            Burn uNOVA in a contract to receive ETH.
+            <br />
+            <i>Fixed rate</i>
+          </div>
+          <div className="backdrop-blur-md backdrop-brightness-50 rounded-sm p-3 my-2">
+            <input
+              className="w-36 my-2 bg-gray-800 rounded-sm p-1"
+              type="number"
+              value={unstakeUnova}
+              onChange={(e) => {
+                if (
+                  isNaN(e.target.value) ||
+                  isNaN(parseFloat(e.target.value))
+                ) {
+                  return;
+                }
+                setUnstakeUnova(e.target.value);
+              }}
+            />
+            &nbsp; <b>uNOVA</b>
+          </div>
+          <div className="flex justify-center">
+            <img
+              src="arrow.svg"
+              width={50}
+              height={50}
+              className="opacity-70"
+            />
+          </div>
+          <div className="backdrop-blur-md backdrop-brightness-50 rounded-sm p-3 my-2">
+            <img src="eth.png" className="inline w-6 m-2"></img>
+            <input
+              className="w-36 my-2 bg-gray-800 rounded-sm cursor-not-allowed p-1"
+              type="number"
+              value={unstakeUnova * 0.002}
+            />
+            &nbsp; <b>ETH</b>&nbsp;&nbsp;&nbsp;&nbsp;
+          </div>
+          <div className="backdrop-blur-md backdrop-brightness-50 rounded-sm p-3 my-2">
+            <img src="aave.svg" className="inline w-6 m-2"></img>
+            <input
+              className="w-36 my-2 bg-gray-800 rounded-sm cursor-not-allowed p-1"
+              type="number"
+              value={unstakeUnova * 0.02}
+            />
+            &nbsp; <b>AAVE</b>&nbsp;
+          </div>
+          <div className="backdrop-blur-md backdrop-brightness-50 rounded-sm p-3 my-2">
+            <img src="mkr.svg" className="inline w-6 m-2"></img>
+            <input
+              className="w-36 my-2 bg-gray-800 rounded-sm cursor-not-allowed p-1"
+              type="number"
+              value={unstakeUnova * 0.002}
+            />
+            &nbsp; <b>MKR</b>&nbsp;&nbsp;
+          </div>
           <br />
-          <i>Fixed rate</i>
+          <button className="backdrop-blur-md backdrop-brightness-90 py-1 w-64 rounded-sm">
+            <mark className="bg-foreground">Unwrap</mark>
+          </button>
         </div>
-        <div className="backdrop-blur-md backdrop-brightness-50 rounded-sm p-3 my-2">
-          <input
-            className="w-36 my-2 bg-gray-800 rounded-sm p-1"
-            type="number"
-            value={unstakeUnova}
-            onChange={(e) => {
-              if (isNaN(e.target.value) || isNaN(parseFloat(e.target.value))) {
-                return;
-              }
-              setUnstakeUnova(e.target.value);
-            }}
-          />
-          &nbsp; <b>uNOVA</b>
-        </div>
-        <div className="flex justify-center">
-          <img src="arrow.svg" width={50} height={50} className="opacity-70" />
-        </div>
-        <div className="backdrop-blur-md backdrop-brightness-50 rounded-sm p-3 my-2">
-          <img src="eth.png" className="inline w-6 m-2"></img>
-          <input
-            className="w-36 my-2 bg-gray-800 rounded-sm cursor-not-allowed p-1"
-            type="number"
-            value={unstakeUnova * 0.002}
-          />
-          &nbsp; <b>ETH</b>&nbsp;&nbsp;&nbsp;&nbsp;
-        </div>
-        <div className="backdrop-blur-md backdrop-brightness-50 rounded-sm p-3 my-2">
-          <img src="aave.svg" className="inline w-6 m-2"></img>
-          <input
-            className="w-36 my-2 bg-gray-800 rounded-sm cursor-not-allowed p-1"
-            type="number"
-            value={unstakeUnova * 0.02}
-          />
-          &nbsp; <b>AAVE</b>&nbsp;
-        </div>
-        <div className="backdrop-blur-md backdrop-brightness-50 rounded-sm p-3 my-2">
-          <img src="mkr.svg" className="inline w-6 m-2"></img>
-          <input
-            className="w-36 my-2 bg-gray-800 rounded-sm cursor-not-allowed p-1"
-            type="number"
-            value={unstakeUnova * 0.002}
-          />
-          &nbsp; <b>MKR</b>&nbsp;&nbsp;
-        </div>
-        <br />
-        <button className="backdrop-blur-md backdrop-brightness-90 py-1 w-64 rounded-sm">
-          <mark className="bg-foreground">Unwrap</mark>
-        </button>
       </div>
     </div>
   );
